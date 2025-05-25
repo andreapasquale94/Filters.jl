dc116d882e8a9b4b13512ffd0b45548b6dd621e0
Andrea Pasquale <andrea.pasquale@polimi.it>
2025-05-25 11:38:18 +0200

    improved examples

---
+++ examples/lkf_forced_oscillator.jl
@@ -0,0 +1,67 @@
+using Filters
+using LinearAlgebra
+using Plots
+using Random
+Plots.gr()
+
+Random.seed!(1)
+T = Float64;
+
+# Include models
+include("models.jl");
+
+# Initial conditions (mean and covariance)
+x̄0 = T[1.0, 0.0];
+P0 = T[0.1 0.0; 0.0 1e-3];
+
+# Create workspace
+work = harmonic_oscillator(
+    T,
+    x̄0;
+    ω0 = 1,
+    γ = 0.1,
+    σₚ = 1e-2,
+    σₘ = 1e-3,
+    ΔT = 0.1,
+    Tf = 50,
+    ω = 0.5
+);
+
+# Create filter 
+kf = KalmanFilter(
+    KalmanState(x̄0, P0),
+    KalmanFilterPrediction{T}(work.model.state, work.model.process_noise),
+    KalmanFilterUpdate{T}(work.model.obs, work.model.obs_noise, work.nx, work.nz)
+);
+
+est = Vector{KalmanState{T}}(undef, work.nt);
+est[1] = deepcopy(kf.est);
+
+# Run filter
+for i in 2:work.nt
+    step!(kf, work.z_sim[:, i]; uk = work.u_sim[i-1])
+    est[i] = deepcopy(kf.est)
+end
+
+# Collect and print results
+t = collect(work.dt);
+x̂ = hcat([estimate(e) for e in est]...);
+xt = work.x_true;
+x = work.x_sim;
+cb = hcat([3sqrt.(diag(covariance(e))) for e in est]...);
+
+# Position plot
+p1 = plot(t, xt[1, :], label = "\$x_{g}(t)\$");
+plot!(p1, t, x[1, :], label = "\$x(t)\$", xlabel = "\$t\$", ylabel = "\$x(t)\$");
+plot!(p1, t, x̂[1, :], label = "\$\\hat{x}(t)\$", ribbon = cb[1, :], fillalpha = 0.15);
+plot!(p1, t, x̂[1, :] + cb[1, :], label = nothing, linestyle = :dash, color = :gray);
+plot!(p1, t, x̂[1, :] - cb[1, :], label = nothing, linestyle = :dash, color = :gray);
+
+# Velocity plot
+p2 = plot(t, xt[2, :], label = "\$x_{g}(t)\$");
+plot!(p2, t, x[2, :], label = "\$x(t)\$", xlabel = "\$t\$", ylabel = "\$v(t)\$");
+plot!(p2, t, x̂[2, :], label = "\$\\hat{x}(t)\$", ribbon = cb[2, :], fillalpha = 0.15);
+plot!(p2, t, x̂[2, :] + cb[2, :], label = nothing, linestyle = :dash, color = :gray);
+plot!(p2, t, x̂[2, :] - cb[2, :], label = nothing, linestyle = :dash, color = :gray);
+
+plot(p1, p2, layout = (2, 1), dpi = 1600, size = (800, 600), framestyle = :box)
--- examples/models.jl
+++ examples/models.jl
@@ -1,68 +1,106 @@
 using LinearAlgebra
 using Filters
 
-function harmonic_oscillator(; ω0, γ, σₚ, σₘ, ΔT, Tf)
-    # Initial state 
-    x0 = [1.0, 0.0]
+function harmonic_oscillator(
+    ::Type{T},
+    x0;
+    ω0,
+    γ,
+    σₚ,
+    σₘ,
+    ΔT,
+    Tf,
+    ω = missing
+) where {T <: Number}
 
-    # ---
-    # Continous-time model
+    # Promote
+    x0 = convert(Vector{T}, x0)
+    ω0 = T(ω0)
+    γ = T(γ)
+    σₚ = T(σₚ)
+    σₘ = T(σₘ)
+    ΔT = T(ΔT)
+    Tf = T(Tf)
+    ω = ismissing(ω) ? missing : T(ω)
 
-    # Damped harmonic oscillator, natural response 
-    # ẍ + 2*γ*ẋ + ω₀²*x = 0
+    # ----
+    # Damped harmonic oscillator
+    # ẍ + 2*γ*ẋ + ω₀²*x = cos(ωt)
+
+    # Continous-time model    
     nx = length(x0)
-    dt = 0.0:ΔT:Tf
-    nt = length(dt)
-    F = [0 1; -ω0^2 -2*γ]    # System matrix
-    H = Float64[1 0]         # Measurement matrix
-    nz = size(H, 1)          # Number of measurements
+    F = T[0 1; -ω0^2 -2*γ]    # System matrix
+    H = T[1 0]                # Measurement matrix
+    nz = size(H, 1)           # Number of measurements
+    B = T[0 1]'               # Input matrix
+
     # Noise
-    Bₚ = [0, 1]
-    Q = Bₚ * σₚ * Bₚ'        # Process noise covariance
-    R = Matrix(σₘ * I(nz))  # Measurement noise covariance
+    Bₚ = T[0, 1]
+    Q = Bₚ * σₚ * Bₚ'          # Process noise covariance
+    R = Matrix{T}(σₘ * I(nz))  # Measurement noise covariance
 
-    # Exact solution
-    exact(t) = exp(F * t) * x0
-    xt = hcat([exact(t) for t in dt]...) # Exact solution
-
-    # ---
-    # Discrete time model
-    Z = [-F Q; zeros(nx, nx) F']
+    # Discrete model
+    Z = T[-F Q; zeros(nx, nx) F']
     C = exp(Z * ΔT)
     Fₖ = Matrix(C[nx+1:end, nx+1:end]')
     Qₖ = Fₖ * C[1:nx, nx+1:end]
     Rₖ = R / ΔT
+    Bₖ = inv(F) * (Fₖ - I(nx)) * B
 
-    # Simulated state and measurements
-    x = zeros(nx, nt + 1)
-    x[:, 1] = x0
-    z = zeros(nz, nt + 1)
-    CQₖ = cholesky(Hermitian(Qₖ)).L
-    CRₖ = cholesky(Hermitian(Rₖ)).L
-    for i in 1:nt
-        x[:, i+1] .= Fₖ * x[:, i] .+ CQₖ * randn(nx)
-        z[:, i] .= H * x[:, i] .+ CRₖ * randn(nz)
-    end
-
-    state = LinearStateModel(Fₖ, Float64[;;])
+    state_model = LinearStateModel(Fₖ, Bₖ)
     obs = LinearObservationModel(H, Float64[;;])
     process_noise = ConstantGaussianNoise(Qₖ)
     obs_noise = ConstantGaussianNoise(Rₖ)
 
+    # Exact solution 
+    natural(t) = exp(F * t) * x0 # Natural response 
+    solution = t -> natural(t)
+    if !ismissing(ω)
+        A_ω = 1 / sqrt((ω0^2 - ω^2) + (2γ * ω)^2) # Forced response amplitudes
+        ϕ_ω = atan(2 * γ * ω / (ω0^2 - ω^2)) # Forced response phase
+        forced(t) = A_ω * [cos(ω * t - ϕ_ω), -ω * sin(ω * t - ϕ_ω)]
+        solution = t -> natural(t) + forced(t)
+    end
+
+    dt = 0.0:ΔT:Tf
+    nt = length(dt)
+    xt = hcat([solution(t) for t in dt]...)
+
+    # Simulated state and measurements
+    x = zeros(T, nx, nt)
+    x[:, 1] = x0
+    z = zeros(T, nz, nt)
+    u = missing
+    if !ismissing(ω)
+        u = cos.(ω * dt)
+    end
+    CQₖ = cholesky(Hermitian(Qₖ)).L
+    CRₖ = cholesky(Hermitian(Rₖ)).L
+
+    for i in 2:nt
+        x[:, i] .= Fₖ * x[:, i-1] .+ CQₖ * randn(nx)
+        if !ismissing(ω)
+            x[:, i] .+= Bₖ * u[i-1]
+        end
+        z[:, i] .= H * x[:, i] .+ CRₖ * randn(nz)
+    end
+
+    # return the workspace
     return (
-        steps = length(dt),
         nx = nx,
         nz = nz,
-        nu = 0,
+        nu = ismissing(ω) ? 0 : 1,
+        nt = nt,
         dt = dt,
         x_true = xt,
         x_sim = x,
-        z = z,
-        models = (
-            state = LinearStateModel(Fₖ, Float64[;;]),
-            obs = LinearObservationModel(H, Float64[;;]),
-            process_noise = ConstantGaussianNoise(Qₖ),
-            obs_noise = ConstantGaussianNoise(Rₖ)
+        z_sim = z,
+        u_sim = u,
+        model = (
+            state = state_model,
+            obs = obs,
+            process_noise = process_noise,
+            obs_noise = obs_noise
         )
     )
 end
